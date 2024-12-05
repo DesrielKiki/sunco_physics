98af1852f3de9ed078957f838a8977d3ab1a0a3d
Desrielkiki <desriel7c6@gmail.com>
2024-12-05 10:16:24 +0700

    :hammer: create history and profile screen

--- lib/presentation/screen/history/history_screen.dart
+++ lib/presentation/screen/history/history_screen.dart
@@ -11,7 +11,7 @@ class _HistoryScreenState extends State<HistoryScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text('History Screen'),
+      body: Center(child: Text('History Screen')),
     );
   }
 }
--- lib/presentation/screen/profile/profile_screen.dart
+++ lib/presentation/screen/profile/profile_screen.dart
@@ -11,7 +11,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text('Profile Screen'),
+      body: Center(child: Text('Profile Screen')),
     );
   }
 }
