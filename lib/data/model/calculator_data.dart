d07b4f9f9ff9dd911c2a251bc323dd7639375073
Desrielkiki <desriel7c6@gmail.com>
2024-12-05 17:13:31 +0700

    :wrench: modify calculator data

--- lib/data/model/calculator_data.dart
+++ lib/data/model/calculator_data.dart
@@ -5,20 +5,32 @@ class CalculatorEntity {
     {
       'title': 'Energi Potensial',
       'icon': Icons.energy_savings_leaf,
-      'route': '/potential_energy'
+      'route': '/potential_energy',
     },
     {
       'title': 'Gaya Gesek',
       'icon': Icons.public,
-      'route': '/gravitational_force'
+      'route': '/gravitational_force',
     },
     {
       'title': 'Gerak Harmonis Sederhana',
       'icon': Icons.build,
-      'route': '/work_done'
+      'route': '/work_done',
     },
-    {'title': 'Momentum', 'icon': Icons.flash_on, 'route': '/power'},
-    {'title': 'Kinematis', 'icon': Icons.sports_mma, 'route': '/momentum'},
-    {'title': 'Listrik', 'icon': Icons.sync, 'route': '/circular_motion'},
+    {
+      'title': 'Momentum',
+      'icon': Icons.flash_on,
+      'route': '/power',
+    },
+    {
+      'title': 'Kinematis',
+      'icon': Icons.sports_mma,
+      'route': '/momentum',
+    },
+    {
+      'title': 'Listrik',
+      'icon': Icons.sync,
+      'route': '/circular_motion',
+    },
   ];
 }
