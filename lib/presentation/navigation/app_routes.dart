a63424baa713b6dadf6c583261bcd5f044e4a4ff
Desrielkiki <desriel7c6@gmail.com>
2024-12-05 17:11:48 +0700

    :wrench: rename lesson list

--- lib/presentation/navigation/app_routes.dart
+++ lib/presentation/navigation/app_routes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sunco_physics/presentation/screen/calculator/calculator/potential_energy.dart';
 import 'package:sunco_physics/presentation/screen/calculator/calculator_list.dart';
 import 'package:sunco_physics/presentation/screen/home/home_screen.dart';
-import 'package:sunco_physics/presentation/screen/lesson/Lesson_list.dart';
+import 'package:sunco_physics/presentation/screen/lesson/lesson_list.dart';
 import 'package:sunco_physics/presentation/screen/splash/splash_screen.dart';
 
 class AppRoutes {
